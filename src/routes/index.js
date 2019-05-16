ceaa2874de6cf2c06e772bbccb3a693875ac0702
Julio Paredes <ing.julio.code@gmail.com>
2019-05-16 13:33:19 -0500

    Site and settings done

---
+++ src/routes/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    res.render('index');
+});
+
+router.post('/add', (req, res) => {
+    console.log(req.body);
+    res.send('recivied');
+    
+});
+
+module.exports = router;
