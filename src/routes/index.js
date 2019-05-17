233c30b4f3622b061119c6c3b968fd84fd467a6c
Julio Paredes <ing.julio.code@gmail.com>
2019-05-17 14:54:02 -0500

    The End

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,14 +1,51 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.render('index');
+//import the Schema
+const Task = require('../models/task');
+
+router.get('/', async (req, res) => {
+    const tasks = await Task.find();
+    //console.log(tasks);
+
+    res.render('index', {
+        tasks
+    });
 });
 
-router.post('/add', (req, res) => {
-    console.log(req.body);
-    res.send('recivied');
+router.post('/add', async (req, res) => {
     
+    const task = new Task(req.body);
+    await task.save();
+
+    res.redirect('/');
+    
+});
+
+router.get('/turn/:id', async (req, res) => {
+    const { id } = req.params;
+    const task = await Task.findById(id);
+    task.status = !task.status;
+    await task.save();
+    res.redirect('/');
+});
+
+router.get('/edit/:id', async (req, res) => {
+    const { id } = req.params;
+    const task = await Task.findById(id);
+    res.render('edit', {task});
+});
+
+router.post('/edit/:id', async (req, res) => {
+    const { id } = req.params;
+    await Task.update({ _id:id }, req.body);
+    res.redirect('/');
+});
+
+router.get('/delete/:id', async (req, res) => {
+    const { id } = req.params;
+    await Task.deleteOne({_id: id});
+    res.redirect('/');
 });
 
 module.exports = router;
